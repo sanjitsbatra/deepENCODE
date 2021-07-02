a97e0f3a653e6fa7ce6b8f7477c3f67aac0ec412
Sanjit Singh Batra <sanjitsbatra@n0225.savio2>
2021-07-02 13:04:32 -0700

    PEP8 baby

--- chrmt_generator.py
+++ chrmt_generator.py
@@ -5,134 +5,131 @@
 # It then creates a generator
 # which contains a masking function
 import numpy as np
-from os.path import join, isfile
+from os.path import isfile
 from tensorflow.python.keras.utils.data_utils import Sequence
 import sys
 from random import randrange
 
 
-DATA_FOLDER = '..//Data/100bp_12_7_Data_20_July_2020' 
+DATA_FOLDER = '../Data/100bp_12_7_Data_20_July_2020'
 
 CELL_TYPES = ["T01", "T05"]
 
 ASSAY_TYPES = ["A02", "A03", "A04", "A05", "A06", "A07"]
 
-training_chroms = ["chr"+str(i) for i in range(18,22,2)] 
+training_chroms = ["chr"+str(i) for i in range(18, 22, 2)]
 
 MASK_VALUE = -1
 
 
 def preprocess_data(data):
 
-	return np.log1p(data)
+    return np.log1p(data)
 
 
 def create_masked(x):
 
-	# dimensions are window_size x len(ASSAY_TYPES)
-	# we mask out some portions by setting them to mask_value
-	for i in range(x.shape[0]):
-		if(np.random.uniform(low=0.0, high=1.0) < 0.95):
-			x[i, :] = MASK_VALUE
+    # dimensions are window_size x len(ASSAY_TYPES)
+    # we mask out some portions by setting them to mask_value
+    for i in range(x.shape[0]):
+        if(np.random.uniform(low=0.0, high=1.0) < 0.95):
+            x[i, :] = MASK_VALUE
 
-	return x
+    return x
 
 
 class DataGenerator(Sequence):
-	
-	def __init__(self, window_size, batch_size, shuffle=True, mode=''):
 
-		self.window_size = window_size
-		self.batch_size = batch_size
-		self.shuffle = shuffle
-		self.mode = mode
+    def __init__(self, window_size, batch_size, shuffle=True, mode=''):
 
-		self.data = {}
-		self.chrom_lens = {}
+        self.window_size = window_size
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.mode = mode
 
-		for chrom in training_chroms:
-			for cell_type in CELL_TYPES:
-				data = []
-				for assay_type in ASSAY_TYPES:
-					fname = cell_type+""+assay_type+"."+chrom+".npy"
-					fname = DATA_FOLDER+"/"+fname
-					if(isfile(fname)):
-						print("Loading", fname, file=sys.stderr)
-						current_data = np.load(fname)
-						current_data = preprocess_data(current_data)
-						data.append(current_data)
-					else:
-						print(assay_type, "missing in", cell_type, chrom)
-						sys.exit(-1)
+        self.data = {}
+        self.chrom_lens = {}
 
-				if(chrom not in self.data):
-					self.data[chrom] = {}
-					self.chrom_lens[chrom] = current_data.shape[0]
-				data = np.vstack(data)	# concatenate all assay types
-				self.data[chrom][cell_type] = data
+        for chrom in training_chroms:
+            for cell_type in CELL_TYPES:
+                data = []
+                for assay_type in ASSAY_TYPES:
+                    fname = cell_type+""+assay_type+"."+chrom+".npy"
+                    fname = DATA_FOLDER+"/"+fname
+                    if(isfile(fname)):
+                        print("Loading", fname, file=sys.stderr)
+                        current_data = np.load(fname)
+                        current_data = preprocess_data(current_data)
+                        data.append(current_data)
+                    else:
+                        print(assay_type, "missing in", cell_type, chrom)
+                        sys.exit(-1)
 
+                if(chrom not in self.data):
+                    self.data[chrom] = {}
+                    self.chrom_lens[chrom] = current_data.shape[0]
+                data = np.vstack(data)  # concatenate all assay types
+                self.data[chrom][cell_type] = data
 
-		# Now we need a way to randomly sample from the genome
-		# For this we need chromosome lengths
-		# We build a mapping from indexes to (chrom, position_in_chrom)
-		self.chrom_list, self.tot_len_list = zip(*self.chrom_lens.items())
-		self.tot_len_list = np.array(self.tot_len_list)
-		self.tot_len_list = np.cumsum(self.tot_len_list)
-		self.idxs = np.arange(self.tot_len_list[-1])
+        # Now we need a way to randomly sample from the genome
+        # For this we need chromosome lengths
+        # We build a mapping from indexes to (chrom, position_in_chrom)
+        self.chrom_list, self.tot_len_list = zip(*self.chrom_lens.items())
+        self.tot_len_list = np.array(self.tot_len_list)
+        self.tot_len_list = np.cumsum(self.tot_len_list)
+        self.idxs = np.arange(self.tot_len_list[-1])
 
+    def __len__(self):
 
-	def __len__(self):
+        return self.tot_len_list[-1] // self.batch_size
 
-		return self.tot_len_list[-1] // self.batch_size
+    def on_epoch_end(self):
 
+        if self.shuffle:
+            np.random.shuffle(self.idxs)
 
-	def on_epoch_end(self):
+    def idx_to_chrom_and_start(self, idx):
 
-		if self.shuffle:
-			np.random.shuffle(self.idxs)
+        chr_idx = np.where(self.tot_len_list > idx)[0][0]
+        chrom = self.chrom_list[chr_idx]
+        start = idx if chr_idx == 0 else idx - self.tot_len_list[chr_idx - 1]
+        return chrom, start
 
+    def __getitem__(self, batch_number):
 
-	def idx_to_chrom_and_start(self, idx):
+        X = np.zeros((self.batch_size, self.window_size, len(ASSAY_TYPES)))
+        Y = np.zeros((self.batch_size, self.window_size, len(ASSAY_TYPES)))
 
-		chr_idx = np.where(self.tot_len_list > idx)[0][0]
-		chrom = self.chrom_list[chr_idx]
-		start = idx if chr_idx == 0 else idx - self.tot_len_list[chr_idx - 1]
-		return chrom, start
+        for i in range(self.batch_size):
+            idx = self.idxs[batch_number * self.batch_size + i]
+            chrom, start = self.idx_to_chrom_and_start(idx)
+            end = start + self.window_size
 
+            if((start < 1000) or (end > self.chrom_lens[chrom] + 1000)):
+                # We are too close to the edges of the chromosome
+                # So we create a dummy point with all 0s
+                # Since X and Y are aleady 0s, we do nothing
+                # TODO: this resets loss; needs to be removed from training
+                print("We are too close to the edge!", file=sys.stderr)
+                pass
+            else:
+                if(self.mode == 'train'):
+                    # Randomly sample a cell type
+                    random_cell_type_index = randrange(len(CELL_TYPES))
+                else:
+                    random_cell_type_index = 0  # Fix cell type for testing
+                random_cell_type = CELL_TYPES[random_cell_type_index]
 
-	def __getitem__(self, batch_number):
+                # TODO: remove this transpose
+                # TODO: add assert on size to make sure it's always consistent
+                y = self.data[chrom][random_cell_type][:, start:end]
+                y = np.transpose(y)
 
-		X = np.zeros((self.batch_size, self.window_size, len(ASSAY_TYPES)))
-		Y = np.zeros((self.batch_size, self.window_size, len(ASSAY_TYPES)))
+                x = create_masked(y)
 
-		for i in range(self.batch_size):
-			idx = self.idxs[batch_number * self.batch_size + i]
-			chrom, start = self.idx_to_chrom_and_start(idx)
-			end = start + self.window_size
+                # print(x.shape, y.shape)
 
-			if( (start < 1000) or (end > self.chrom_lens[chrom] + 1000) ):
-				# We are too close to the edges of the chromosome
-				# So we create a dummy point with all 0s
-				# Since X and Y are aleady 0s, we do nothing
-				# TODO: this resets loss; needs to be removed from training
-				print("We are too close to the edge!", file=sys.stderr)
-				pass
-			else:
-				if(self.mode == 'train'):
-					# Randomly sample a cell type
-					random_cell_type_index = randrange(len(CELL_TYPES))
-				else:
-					random_cell_type_index = 0 # Fix cell type for testing
-				random_cell_type = CELL_TYPES[random_cell_type_index]
+                X[i] = x
+                Y[i] = y
 
-				# TODO: remove this transpose
-				# TODO: add assert on size to make sure it's always consistent
-				y = np.transpose(self.data[chrom][random_cell_type][:,start:end])
-				x = create_masked(y)
-
-				# print(x.shape, y.shape)
-
-				X[i] = x
-				Y[i] = y
-
-		return X, Y
+        return X, Y
--- chrmt_train.py
+++ chrmt_train.py
@@ -1,148 +1,118 @@
-import sys, os
-import numpy as np
-import tensorflow, keras
-from chrmt_generator import DataGenerator, CELL_TYPES, ASSAY_TYPES, MASK_VALUE
-from keras.models import load_model
+import sys
+import os
+from chrmt_generator import DataGenerator, ASSAY_TYPES, MASK_VALUE
 from keras.callbacks import LearningRateScheduler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.layers import BatchNormalization, Input, add
-from tensorflow.keras.layers import Conv1D, MaxPooling1D, AveragePooling1D
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
-from tensorflow.keras.layers import Activation, Dropout, Dense, Flatten
-from tensorflow.keras import optimizers
+from tensorflow.keras.models import Model
+# from tensorflow.keras.layers import BatchNormalization, Input, add
+from tensorflow.keras.layers import Conv1D, Input
+# from tensorflow.keras.layers import Activation, Dropout, Dense, Flatten
+# from tensorflow.keras import optimizers
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from tensorflow.keras import losses
+from tensorflow.keras.callbacks import ModelCheckpoint
 from keras import backend as K
-from time import time
-import random
-from tqdm import tqdm 
+# from tqdm import tqdm
 
 
 def lr_scheduler(epoch):
-	if epoch < 10:
-		return 2e-3
-	elif epoch < 90:
-		return 1e-3
-	else:
-		return 5e-4
-
-
-def BACUnit(conv_kernel_size, num_filters, padding):
-	def f(input_node):
-		bn = BatchNormalization()(input_node)
-		act = Activation('relu')(bn)
-		output_node = Conv1D(kernel_size=conv_kernel_size, 
-							filters=num_filters, 
-							padding=padding)(act)
-		return output_node
-	return f
-
-
-def ResidualUnit(conv_kernel_size, num_filters, padding):
-	def  f(input_node):
-		bac1 = BACUnit(conv_kernel_size, num_filters, padding)(input_node)
-		bac2 = BACUnit(conv_kernel_size, num_filters, padding)(bac1)
-		# if(padding == 'valid'):
-		# 	input_node = Cropping1D(conv_kernel_size - 1)(input_node)
-		output_node = add([input_node, bac2]) 
-		return output_node
-	return f
+    if epoch < 10:
+        return 2e-3
+    elif epoch < 90:
+        return 1e-3
+    else:
+        return 5e-4
 
 
 # Compute an MSE loss only at those positions that are 0
 def custom_loss(yTrue, yPred):
-	print(yPred)
+    print(yPred)
 
-	masked_indices = K.tf.where(K.tf.equal(yTrue, MASK_VALUE),
-								K.tf.ones_like(yTrue),
-								K.tf.zeros_like(yTrue))
+    masked_indices = K.tf.where(K.tf.equal(yTrue, MASK_VALUE),
+                                K.tf.ones_like(yTrue),
+                                K.tf.zeros_like(yTrue))
 
-	loss = K.square(yTrue-yPred) * masked_indices
-	return K.sum(loss, axis=-1) / K.sum(masked_indices, axis=-1)
+    loss = K.square(yTrue-yPred) * masked_indices
+    return K.sum(loss, axis=-1) / K.sum(masked_indices, axis=-1)
 
 
 def create_cnn(number_of_assays,
-			window_size, 
-			num_filters, 
-			conv_kernel_size, 
-			num_convolutions, 
-			padding):
-	inputs = Input(shape=(window_size, number_of_assays), name='input') 
+               window_size,
+               num_filters,
+               conv_kernel_size,
+               num_convolutions,
+               padding):
+    inputs = Input(shape=(window_size, number_of_assays), name='input')
 
-	print("Initial", inputs)
+    print("Initial", inputs)
 
-	x = inputs
-	# Initial convolution
-	x = Conv1D(kernel_size=conv_kernel_size, 
-			filters=num_filters, 
-			padding=padding)(x)
+    x = inputs
+    # Initial convolution
+    x = Conv1D(kernel_size=conv_kernel_size,
+               filters=num_filters,
+               padding=padding)(x)
 
-	print("After initial", x)
+    print("After initial", x)
 
-	# Perform multiple convolutional layers
-	for i in range(num_convolutions):
-		x = ResidualUnit(conv_kernel_size, num_filters, padding)(x)
-		# x = Conv1D(kernel_size=conv_kernel_size,
-		# 			filters=num_filters,
-		# 			padding=padding)(x)
+    # Perform multiple convolutional layers
+    for i in range(num_convolutions):
+        x = Conv1D(kernel_size=conv_kernel_size,
+                   filters=num_filters,
+                   padding=padding)(x)
 
-	print("After multiple", x)
+    print("After multiple", x)
 
-	# Final convolution
-	outputs = Conv1D(kernel_size=conv_kernel_size,
-				filters=number_of_assays,
-				padding=padding)(x)
+    # Final convolution
+    outputs = Conv1D(kernel_size=conv_kernel_size,
+                     filters=number_of_assays,
+                     padding=padding)(x)
 
-	print("After final", outputs)
-	K.print_tensor(outputs, message='After final')
+    print("After final", outputs)
+    K.print_tensor(outputs, message='After final')
 
-	# construct the CNN
-	model = Model(inputs=inputs, outputs=outputs)
-	return model
+    # construct the CNN
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
 
 
 if __name__ == '__main__':
 
-	epochs = 100     
-	steps_per_epoch = 100
+    epochs = 100
+    steps_per_epoch = 100
 
-	run_name_prefix = sys.argv[1]
-	window_size = int(sys.argv[2]) # => Length of window / 100bp 
-	batch_size = int(sys.argv[3])
-	num_filters = int(sys.argv[4])
-	conv_kernel_size = 7
-	num_convolutions = int(sys.argv[5])
-	padding = 'same'
+    run_name_prefix = sys.argv[1]
+    window_size = int(sys.argv[2])  # => Length of window / 100bp
+    batch_size = int(sys.argv[3])
+    num_filters = int(sys.argv[4])
+    conv_kernel_size = 7
+    num_convolutions = int(sys.argv[5])
+    padding = 'same'
 
-	run_name = run_name_prefix+"_"+str(window_size)+"_"+str(num_filters)+"_"+str(num_convolutions)
+    run_name = (run_name_prefix + "_" + str(window_size) +
+                "_" + str(num_filters) + "_" + str(num_convolutions))
 
-	# tensorflow.enable_eager_execution()
+    # tensorflow.enable_eager_execution()
 
-	train_generator = DataGenerator(window_size, 
-							batch_size, 
-							shuffle=True, 
-							mode='train')
+    train_generator = DataGenerator(window_size,
+                                    batch_size,
+                                    shuffle=True,
+                                    mode='train')
 
-	model = create_cnn(len(ASSAY_TYPES),
-					window_size, 
-					num_filters, 
-					conv_kernel_size, 
-					num_convolutions, 
-					'same')
-	checkpoint = ModelCheckpoint(run_name+"."+"model-{epoch:02d}.hdf5",
-								verbose=0, save_best_only=False)
-	lr_schedule = LearningRateScheduler(lr_scheduler)
-	model.compile(loss='mse', optimizer=Adam(clipnorm=1.), run_eagerly=True)
-	print(model.summary())
+    model = create_cnn(len(ASSAY_TYPES),
+                       window_size,
+                       num_filters,
+                       conv_kernel_size,
+                       num_convolutions,
+                       'same')
+    checkpoint = ModelCheckpoint(run_name+"."+"model-{epoch:02d}.hdf5",
+                                 verbose=0, save_best_only=False)
+    lr_schedule = LearningRateScheduler(lr_scheduler)
+    model.compile(loss='mse', optimizer=Adam(clipnorm=1.), run_eagerly=True)
+    print(model.summary())
 
-	model.fit_generator(train_generator, 
-						epochs=epochs,
-						steps_per_epoch=steps_per_epoch,
-						callbacks=[checkpoint, lr_schedule],
-						use_multiprocessing=False, 
-						verbose=2)
+    model.fit_generator(train_generator,
+                        epochs=epochs,
+                        steps_per_epoch=steps_per_epoch,
+                        callbacks=[checkpoint, lr_schedule],
+                        use_multiprocessing=False,
+                        verbose=2)
 
-	os._exit(1)
-
+    os._exit(1)
